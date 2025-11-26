0c8a399b8264722cb6a98d93f6d8c24c6a9bbf64
Christoph Strasen <1693413+christophstrasen@users.noreply.github.com>
2025-11-26 20:03:00 +0100

    Expands tests

--- tests/unit/query_rewrap_spec.lua
+++ tests/unit/query_rewrap_spec.lua
@@ -92,4 +92,69 @@ describe("Query rewrap from builder sources", function()
 		subject:onNext({ id = 1 })
 		assert.is_true(#bucket >= 1)
 	end)
+
+	it("rewraps group_enriched with schema override and resetTime", function()
+		local subject = rx.Subject.create()
+		local now = 999
+		local bucket = {}
+
+		Query.from(subject, {
+			schema = "enriched_stream",
+			resetTime = true,
+			timeField = "sourceTime",
+			currentFn = function()
+				return now
+			end,
+		}):into(bucket):subscribe()
+
+		subject:onNext({
+			_count = 2,
+			sourceTime = 1,
+			RxMeta = {
+				shape = "group_enriched",
+				groupKey = "gk",
+				groupName = "animals",
+			},
+			animals = {
+				value = 5,
+				RxMeta = { schema = "animals", id = 7 },
+			},
+			["_groupBy:animals"] = {
+				_count = 2,
+				RxMeta = {
+					schema = "_groupBy:animals",
+					groupKey = "gk",
+					groupName = "animals",
+				},
+			},
+		})
+
+		local res = bucket[1]
+		assert.is_true(getmetatable(res) == Result)
+		assert.are.equal(now, res:get("animals").sourceTime)
+		assert.are.equal("animals", res.RxMeta.schemaMap["animals"].schema)
+	end)
+
+	it("honors idField override when rewrapping grouped aggregate", function()
+		local subject = rx.Subject.create()
+		local bucket = {}
+
+		Query.from(subject, {
+			schema = "agg_schema",
+			idField = "customId",
+		}):into(bucket):subscribe()
+
+		subject:onNext({
+			customId = "cid-1",
+			RxMeta = {
+				shape = "group_aggregate",
+				groupKey = "g1",
+				groupName = "agg_schema",
+			},
+		})
+
+		local payload = bucket[1]:get("agg_schema")
+		assert.are.equal("cid-1", payload.RxMeta.id)
+		assert.are.equal("customId", payload.RxMeta.idField)
+	end)
 end)
